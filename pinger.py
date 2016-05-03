7fa8fc699c8bc327856f28dbb0245e7bd8a2b1d2
Ben Anderson <dataknut@icloud.com>
2016-05-04 00:18:18 +0100

    fixed a typo and added more accurate timestamp

--- pinger.py
+++ pinger.py
@@ -20,7 +20,7 @@ import sys
 
 # how long to run for in minutes?
 duration = float(sys.argv[2])
-# duration between pings (seconds)
+# gap between pings (seconds)
 gap = float(sys.argv[3])
 
 
@@ -49,22 +49,24 @@ while True:
 	if time.time() - (started*60) < duration: # check against duration which is in minutes
 		with open(ofile, "a") as theFile:
 			for url in urls:
+				now = datetime.datetime.now().strftime(format = "%Y-%m-%d %H:%M:%S")
 				p = subprocess.Popen(['ping','-c','1', url[1]], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 				out, err = p.communicate()
 				try:
 					ms = out.split("\n")[1].split("time=")[-1].split(" ")[0]	
 					# set a nice datetime for R etc	
 					# allow for error column at the end																																																																				
-					out = datetime.datetime.now().strftime(format = "%Y-%m-%d %H:%M:%S") + "," + url[0] + "," + ms + "," + "OK"
+					out =  now + "," + url[0] + "," + ms + "," + "OK"
 				except IndexError:
-					# Error in indexing the split list
+					# Error in indexing the split list from ping
 					# Probably no internet connection at all so ping has failed
 					# set the error message to be output
 					# need to remove \n from the end of the error string
 					error = ("%s" % err)
 					# set a nice datetime for R etc
 					# leave ms empty & add error
-					out = datetime.datetime.now().strftime(format = "%Y-%m-%d %H:%M:%S") + "," + url[0] + "," + "," + error.split("\n")[0]
+					out = now + "," + url[0] + "," + "," + error.split("\n")[0]
+				# there are a load of other ping error responses we really ought to catch properly
 				theFile.write(out + "\n")
 
 				out = out.split(",")
